1b52a95bdaa01464aaad6445d0312ac5cd6dbbf1
unknown <hobsond26@icloud.com>
2020-05-26 12:23:54 -0400

    upadted the components list and added a state for the userData

--- silentauction/src/App.js
+++ silentauction/src/App.js
@@ -4,19 +4,31 @@ import {Route,Switch, Redirect} from 'react-router-dom'
 import {UserContext} from './utils/context'
 
 import './App.css';
+import Nav from './components/Nav'
 import Landing from './components/Landing'
 import Login from './components/Login'
+import Seller from './components/Seller'
+import Bidder from './components/Bidder'
+
+import PrivateRoute from './utils/PrivateRoute'
+
 
 function App() {
+  const [userData,setUserData]= useState({})
+
   return (
     <div className="App">
-      <UserContext.Provider value={{}}>
+      <UserContext.Provider value={{userData}}>
          
       <Switch>
 
-
+    {localStorage.getItem('role') === false
+     ? <PrivateRoute path='/home' component={Seller}/>
+    : <PrivateRoute path="/home" component={Bidder}/>
+    }
     <Route path='/login' component={Login}/>
-    <Route path='/' component={Landing} />
+    <Route path='/welcome' component={Landing} />
+    <Redirect to='/welcome'/>
     
       </Switch>
       </UserContext.Provider>
--- silentauction/src/utils/PrivateRoute.js
+++ silentauction/src/utils/PrivateRoute.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import {Route, Redirect} from 'react-router-dom'
 
-const PrivateRoute =({component:Component,...rest})=>{
-    <Route {...rest} render={
-        props=>{
-            localStorage.getItem('token') ? (
-                <Component {...props}/>
-            )
-            :
-            (<Redirect to='/login'/>)
-        }
-    } />
-}
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+  export default PrivateRoute
