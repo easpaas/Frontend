396b24ba7ca59a8b6a372cdc574acb4a1a42fd87
unknown <hobsond26@icloud.com>
2020-05-26 10:13:22 -0400

    created axiosWith auth,PrivateRoute ,and context utils

--- silentauction/src/App.js
+++ silentauction/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useContext} from 'react';
+import {Route,Switch, Redirect} from 'react-router-dom'
+
+import {UserContext} from './utils/context'
+
 import './App.css';
+import Landing from './components/Landing'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value={{}}>
+         
+      <Switch>
+
+
+    <Route path='/login' component={Login}/>
+    <Route path='/' component={Landing} />
+    
+      </Switch>
+      </UserContext.Provider>
+     
+
     </div>
   );
 }
