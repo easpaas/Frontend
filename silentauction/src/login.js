d7f1e84b7993b8e209043d1c0530c9c96d4f7ef7
Impulse2020 <irishht@gmail.com>
2020-05-26 16:24:43 -0400

    Creates mock up components

--- silentauction/src/login.js
+++ silentauction/src/login.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function LogMeIn(){
 
+return(
+    <div className="form Login">
+        <h1>Log in!</h1>
+        <h3>Name:</h3>
+        <h3>Password:</h3>
+        <button></button>
+    </div>  
 
+
+
+)
 
 }
 
--- silentauction/src/registration.js
+++ silentauction/src/registration.js
@@ -3,8 +3,22 @@ import React from 'react'
 function Register(){
 
 
+    return(
+        <div className="form Register" >
+            <h1>Register below:</h1>
+            <h3>Name:</h3>
+            <h3>Email:</h3>
+            <h3>Password:</h3>
+            <button></button>
+        </div>
+    )
 
 
 }
+
+
+
+
+
 
 export default Register
