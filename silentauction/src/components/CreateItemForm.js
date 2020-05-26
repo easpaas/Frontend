e7f02b3ad4891a5fa3c25330e505877e4b980828
unknown <hobsond26@icloud.com>
2020-05-26 16:00:58 -0400

    added form validation for creating  a new item

--- silentauction/src/components/CreateItemForm.js
+++ silentauction/src/components/CreateItemForm.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React,{useState} from 'react'
+import * as Yup from 'yup'
 
 export default function CreateItemForm() {
+
+    const initial = {
+        title:'',
+        category:'',
+        description:'',
+        openDate: Date.now(),
+        closeDate:'',
+        startingBid:'',
+
+    }
+
+    const [formValue,setformValue] = useState(initial)
+    const [erros,setErrors]= useState(initial)
+    const [formValid,setFormValid] = useState(false)
+
+    useEffect(()=>{
+        formScheme.isValid(formValue)
+        .then(setFormValid(!formValid))
+    },
+    [formValue])
+
+    const changeHandle = evt=>{
+        setformValue({...formValue,[evt.target.name] : evt.target.value})
+    }
+    const formScheme = Yup.object().shape({
+        title: Yup 
+        .string()
+        .min(5,'Title must be at least 5 characters')
+        .max(25, 'Max length is 25 characters'),
+        category: Yup
+        .string()
+        .required(' 😺You must select a category 😺'),
+        description:Yup
+        .string()
+        .notRequired()
+        .max(140,'You have exceded the limit for characters in description'),
+        closeDate: Yup
+        .date()
+        
+        .required('You must choose a day for bidding to close '),
+        startingBid: Yup
+        .number()
+        .required('Must enter a start date greater than 0')
+        .moreThan(0,'Cannot set bid for 0')
+
+
+    })
+
+
     return (
         <form>
             <input name='title'type='text ' />
-            <select name='category'>
+            <select onChange={changeHandle} name='category'>
                 <option >select category</option>
                 <option value='electronics'>Electronics</option>
                 <option value='home'>Home and Furniture</option>
@@ -12,11 +62,11 @@ export default function CreateItemForm() {
                 <option value='collectorItems'>Collectors Items</option>
             </select>
 
-            <textarea name='description' />
+            <textarea  onChange={changeHandle} name='description' />
 
-            <input type='date' name='dateOpened'/>
-            <input type='date' name='dateClosed'/>
-            <input type='number' name='startingBid'/>
+            <input  onChange={changeHandle} type='date' name='dateOpened'/>
+            <input  onChange={changeHandle} type='date' name='dateClosed'/>
+            <input  onChange={changeHandle} type='number' name='startingBid'/>
 
             
         </form>
