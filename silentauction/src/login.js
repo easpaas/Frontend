f4dd19cf587114d02acb9f6dd2c598616347fe51
Impulse2020 <irishht@gmail.com>
2020-05-27 13:37:21 -0400

    adds proper form values and state

--- silentauction/src/login.js
+++ silentauction/src/login.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const initialFormValues = {
+    name: "",
+    password:""
+
+}
+
+
+
+
 
 function LogMeIn(props){
-    const{
-        values,
-        onInputChange,
-        onSubmit,
-        diabled,
-        errors,
-    } = props
-
+const [formValues,setFormValues] = useState(initialFormValues);
 
 return(
     <div className="form Login">
         <h1>Log in!</h1>
-        <h3>Name:<input name="name" type="text" value={values.username}/></h3>
-        <h3>Password:<input name="password" type="password" value={values.password} /></h3>
+        <h3>Name:<input name="name" type="text" /*value={'values.username'} */ /></h3>
+        <h3>Password:<input name="password" type="password" /*value={'values.password'} */ /></h3>
         <button onSubmit={login}>Log in</button>
         {/*The login form!*/}
     </div>  
--- silentauction/src/registration.js
+++ silentauction/src/registration.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const initialFormValues = {
+    name: '',
+    Email: '',
+    password:'',
+    seller: 'false'
+}
+
+const submit =(event) =>{
+    
+}
+const onChange = (event) => {
+
+}
 
 function Register(props){
-    const{
-        values,
-        onInputChange,
-        onSubmit,
-        diabled,
-        errors,
-    } = props
+   const [formValues, setFormValues] = useState(initialFormValues);
 
     return(
         <div className="form Register" >
-            <h1>Register below:</h1>
-            <h3>Name:<input name="name" type="text" value ={values.username}/></h3>
-            <h3>Email: <input name="name" type="text" value={values.email} /></h3>
-            <h3>Password:<input name="password" type="password" value={values.password} /></h3>
+            <label>Register below:</h1>
+            <label>Name:<input name="name" type="text" value ={formValues.username} /></label>
+            <label>Email: <input name="name" type="text" value={formValues.email} /></label>
+            <label>Password:<input name="password" type="password" value={formValues.password} /></label>
+            <label>Seller?<input name="seller" /></label>
             <button onSubmit={submit}> Register!</button>
             {/*The registration form!*/}
         </div>
@@ -23,13 +33,6 @@ function Register(props){
 
 }
 
-function submit(){
-   
-
-
-
-    {/*will post the users info into the database */}
-}
 
 
 
