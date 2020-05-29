9ca92b51ab567664d466b42a90cd943d80df1855
Impulse2020 <irishht@gmail.com>
2020-05-29 11:49:15 -0400

    Trying to resolve conflicts part 2

---
+++ silentauction/src/App.js
@@ -0,0 +1,78 @@
+import React,{useState,useContext, useEffect} from 'react';
+import {Route,Switch, Redirect} from 'react-router-dom'
+import axios from 'axios'
+
+
+
+import './App.css';
+import Nav from './components/Nav'
+import Landing from './components/Landing'
+import Login from './components/Login'
+import Register from './components/Register'
+import Seller from './components/Seller'
+import Bidder from './components/Bidder'
+
+import PrivateRoute from './utils/PrivateRoute'
+import {UserContext,GlobalContext, SellerContext, BidderContext} from './utils/context'
+
+import itemFunctions from './utils/itemRouteFunctions'
+
+
+function App() {
+  
+  const [userData,setUserData]= useState({
+    username:'bidguy',
+    password:'thiss',
+    //  seller:"false" , 
+     createdBids:[{
+       id:2,
+      startingBid:200,title:'fishnet',description:'looks good',category:'electronics',bidders:[]}],
+     watchlist:[]})
+  const [globalData,setglobalData]=useState({})
+  const [sellerData,setSellerData] =useState({})
+  const [bidderData,setBidderData] =useState({bids:[{
+    id:1,
+    title:'ricks ticks',
+    startingBid:300,
+    closeDate:'12/4/2020',
+    description:'smells great',
+    category:'outside',
+    bidders:[],
+    currentBid:300
+    
+
+
+}],
+
+})
+
+
+
+  return (
+    <div className="App">
+      <GlobalContext.Provider value={{globalData,setglobalData,itemFunctions}}> 
+          <UserContext.Provider value={{userData,setUserData}}>
+            <SellerContext.Provider value={{sellerData,setSellerData}}>
+              <BidderContext.Provider value={{bidderData,setBidderData}}>
+                  
+                <Switch>
+
+                  {localStorage.getItem('seller') === 'true'
+                  ? <PrivateRoute path={`/home/${userData.username}`} component={Seller}/>
+                  : <PrivateRoute path={`/home/${userData.username}`} component={Bidder}/>
+                  }
+                  <Route path='/login' component={Login}/>
+                  <Route path='/register' component={Register}/>
+                  <Redirect to='/login'/>
+              
+                </Switch>
+              </BidderContext.Provider>
+            </SellerContext.Provider>
+          </UserContext.Provider>
+      </GlobalContext.Provider>
+
+    </div>
+  );
+}
+
+export default App;
