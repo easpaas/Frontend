0f3312d8daaee40770004b36bfe27fb19f81c1c2
Impulse2020 <irishht@gmail.com>
2020-05-29 12:11:29 -0400

    test

--- silentauction/src/App.js
+++ silentauction/src/App.js
@@ -1,76 +1,41 @@
-import React,{useState,useContext, useEffect} from 'react';
-import {Route,Switch, Redirect} from 'react-router-dom'
-import axios from 'axios'
-
-
-
+import React , {useState} from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav'
-import Landing from './components/Landing'
-import Login from './components/Login'
-import Register from './components/Register'
-import Seller from './components/Seller'
-import Bidder from './components/Bidder'
+import LogMeIn from './login';
+import Register from './registration';
 
-import PrivateRoute from './utils/PrivateRoute'
-import {UserContext,GlobalContext, SellerContext, BidderContext} from './utils/context'
 
-import itemFunctions from './utils/itemRouteFunctions'
+
+const initialFormValues= {
+  username: '',
+  email:'',
+  password: '',
+  /*text inputs*/
+  seller:'false',
+  /*seller status could be radio or dropdown*/
+
+
+
+}
+
 
 
 function App() {
-  
-  const [userData,setUserData]= useState({
-    username:'bidguy',
-    password:'thiss',
-    //  seller:"false" , 
-     createdBids:[{
-       id:2,
-      startingBid:200,title:'fishnet',description:'looks good',category:'electronics',bidders:[]}],
-     watchlist:[]})
-  const [globalData,setglobalData]=useState({})
-  const [sellerData,setSellerData] =useState({})
-  const [bidderData,setBidderData] =useState({bids:[{
-    id:1,
-    title:'ricks ticks',
-    startingBid:300,
-    closeDate:'12/4/2020',
-    description:'smells great',
-    category:'outside',
-    bidders:[],
-    currentBid:300
-    
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [registering, setRegistering] = useState({});
+  const [loggingIn, setLoggingIn] = useState(false);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [disabled, setDisabled] = useState(false);
+{/*Sets up some states!*/}
 
-
-}],
-
-})
 
 
 
   return (
     <div className="App">
-      <GlobalContext.Provider value={{globalData,setglobalData,itemFunctions}}> 
-          <UserContext.Provider value={{userData,setUserData}}>
-            <SellerContext.Provider value={{sellerData,setSellerData}}>
-              <BidderContext.Provider value={{bidderData,setBidderData}}>
-                  
-                <Switch>
-
-                  {localStorage.getItem('seller') === 'true'
-                  ? <PrivateRoute path={`/home/${userData.username}`} component={Seller}/>
-                  : <PrivateRoute path={`/home/${userData.username}`} component={Bidder}/>
-                  }
-                  <Route path='/login' component={Login}/>
-                  <Route path='/register' component={Register}/>
-                  <Redirect to='/login'/>
-              
-                </Switch>
-              </BidderContext.Provider>
-            </SellerContext.Provider>
-          </UserContext.Provider>
-      </GlobalContext.Provider>
-
+    
+    
+      
     </div>
   );
 }
